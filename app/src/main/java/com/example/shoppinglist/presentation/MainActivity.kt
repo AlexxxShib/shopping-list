6b31781b7cd7fc2a36b8637bd0623799a4527785
Aleksei Shibkov <alexxxshib@gmail.com>
2023-12-22 18:11:49 +0100

    init

---
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.shoppinglist.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.shoppinglist.databinding.ActivityMainBinding
+import com.example.shoppinglist.ui.theme.ShoppingListTheme
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
